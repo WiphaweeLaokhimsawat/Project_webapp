9f287e2b3f61a07b5e5fa2f9f965b885085ae822
Pruk12 <chanitpol.s@ku.th>
2024-10-14 14:42:36 +0700

    newwwww

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menupage from './Screen/Menupage';
+import FirstPage from './Screen/FirstPage';
+import Login from './Screen/LoginPage';
+import Register from './Screen/RegisterPage';
+import Home from './Screen/HomePage';
+import About from './Screen/AboutPage';
+import TablePage from './Screen/TablePage';
+import Chefpage from './Screen/Chefpage';
+import TableBooking from './Screen/TableBooking';
+import SetTime from './Screen/SetTime';
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route index element={<FirstPage/>}/>
+        <Route path="login" element={<Login/>} />
+        <Route path="register" element={<Register/>}/>
+        <Route path="home" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="menupage" element={<Menupage/>}/>
+        <Route path="tablepage" element={<TablePage/>}/>
+        <Route path="chefpage" element={<Chefpage/>}/>
+        <Route path="tablebooking" element={<TableBooking/>}/>
+        <Route path="settime" element={<SetTime/>}/>
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+// dfds
+export default App;
